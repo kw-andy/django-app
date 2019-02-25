f601d96bfc634953accdd7c076090b71eff023c4
kw-andy <andy.kwok.work@gmail.com>
2019-02-25 17:04:04 +0000

    adding a few changes in the view

--- enquete/urls.py
+++ enquete/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from django.conf.urls import url
 from . import views
 
+app_name = 'enquete'
 urlpatterns = [
     path('',views.index, name='index'),
+    path('<int:question_id>/',views.detail,name='detail'),
+    path('<int:question_id>/results',views.results,name='results'),
+    path('<int:question_id/vote/',views.vote,name='vote'),
 ]
 
 #urlpatterns = [
